2da2db29e9bfe44e1be80b84a6e50dd321c222d8
Dave Barnett <dave.barnett@redsensors.com>
2018-01-04 09:45:55 +0000

    Basic Plane with sample map image

---
+++ Maps/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class MapGenerator : MonoBehaviour
+{
+	public Text gameText;
+
+	void Start()
+	{
+		Debug.Log(string.Format("Location enabled? {0}", Input.location.isEnabledByUser));
+	}
+	
+	void Update()
+	{
+	}
+}
